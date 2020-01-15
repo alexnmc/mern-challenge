fcef5042bbb8f3331e3a7d83cf685778c42632fd
alex <nemechekalexander@gmail.com>
2020-01-15 13:36:27 -0600

    update

--- client/src/App.js
+++ client/src/App.js
@@ -113,8 +113,8 @@ class App extends Component {
         <div className ='pie'>
           <DonutChart
             colors = {[ '#0051ff', '#00ff40', '#ff3c00', '#673ab7', '#3f51b5', '#2196f3', '#03a9f4', '#00bcd4', '#009688', '#4caf50', '#8bc34a', '#cddc39', '#ffeb3b', '#ffc107', '#ff9800', '#ff5722', '#795548', '#607d8b' ]	}
-            innerRadius={0.35}
-            outerRadius={0.60}
+            innerRadius={0.37}
+            outerRadius={0.55}
             height={document.documentElement.clientWidth < 900 ? 250 : 500}
             width={document.documentElement.clientWidth < 900 ? 325 : 750}
             // on mobile view we display only the initials in the doghnut chart, to fit the screen
