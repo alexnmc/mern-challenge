e90eb1b80e80d5d805ed82921e0b4b0614158b0e
alex <nemechekalexander@gmail.com>
2020-01-14 18:12:27 -0600

    deploying

--- server.js
+++ server.js
@@ -1,27 +1,51 @@
 const express = require('express')
+const express = require('express')
 const app = express()
-const path = require("path")
+require ('dotenv').config()
+const morgan = require('morgan')
 const mongoose = require('mongoose')
-const PORT =  8000
+const expressJwt = require("express-jwt");
+const path = require("path")
+const PORT = process.env.PORT || 8000
+const secret = process.env.SECRET || "some secret passphrase here for local development"
+
 
 
 app.use(express.json()) 
-app.use("/data", require("./routes/data"))
-
+app.use(morgan('dev'))  
+app.use("/api", expressJwt({secret})) //req.user === {username, password, _id}
 app.use(express.static(path.join(__dirname, "client", "build")))
+
+
+
+app.use("/data", require("./routes/data"))
 
 
 
 
 mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/fullstack', {useNewUrlParser: true}, () => {
-    console.log('connect to the db captain!')    // name of database is fullstack
+    console.log('connect to the db captain!')    
 })
 mongoose.set('useCreateIndex', true); // stops the error message...
+
+
+
+app.use((err, req, res, next) => {
+    if (err.name === "UnauthorizedError") {
+        
+        res.status(err.status); //secret error 
+    }
+    return res.send({errMsg: err.message})
+})
+
+
 
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"))
 });
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on Port ${PORT} sir!`)
