4460d45adbe17dcd7699cbbc1b610fbb5d7dc238
alex <nemechekalexander@gmail.com>
2020-01-15 15:38:37 -0600

    responsive design

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
     }
   }
 
-  onResize = () =>{if(document.documentElement.clientWidth < 900 || document.documentElement.clientWidth > 900 ){return window.location.reload()}}
+  onResize = () => {if(document.documentElement.clientWidth < 1200){return window.location.reload()}}
   //refresh the page for the mobile view
   
   componentDidMount(){
@@ -52,7 +52,7 @@ class App extends Component {
         })
       }
     }else{
-      alert("no data")
+      alert("No Data")
     }
   }
 
@@ -97,7 +97,8 @@ class App extends Component {
             <h2 className = 'title'>Last Name</h2>
             <h2 className = 'title'>Participation</h2>
           </div>
-            {this.state.data.sort(function (a, b) {    // sorting data in increasing order
+            {
+              this.state.data.sort(function (a, b) {    // sorting data in increasing order
                 return a.participation - b.participation
               }).map((item, index) => {
                 return(
@@ -112,14 +113,14 @@ class App extends Component {
         </div>
         <div className ='pie'>
           <DonutChart
-            colors = {[ '#0051ff', '#00ff40', '#ff3c00', '#673ab7', '#3f51b5', '#2196f3', '#03a9f4', '#00bcd4', '#009688', '#4caf50', '#8bc34a', '#cddc39', '#ffeb3b', '#ffc107', '#ff9800', '#ff5722', '#795548', '#607d8b' ]	}
-            innerRadius={0.37}
-            outerRadius={0.55}
-            height={document.documentElement.clientWidth < 900 ? 250 : 500}
-            width={document.documentElement.clientWidth < 900 ? 325 : 750}
+            colors = {[ '#0051ff', '#00ff40', '#ff3c00', '#673ab7', '#3f51b5', '#2196f3', '#03a9f4', '#00bcd4', '#009688', '#4caf50', '#8bc34a', '#cddc39', '#ffeb3b', '#ffc107', '#ff9800', '#ff5722', '#795548', '#607d8b' ]}
+            innerRadius={0.38}
+            outerRadius={0.56}
+            height={document.documentElement.clientWidth < 1200 ? 250 : 500}
+            width={document.documentElement.clientWidth < 1200 ? 325 : 750}
             // on mobile view we display only the initials in the doghnut chart, to fit the screen
             data={
-              document.documentElement.clientWidth < 900 ?
+              document.documentElement.clientWidth < 1200 ?
               this.state.data.map(item => item = {label: item.firstName[0]+'.'+item.lastName[0]+'.', value: item.participation})
               :
               this.state.data.map(item => item = {label: item.firstName+' '+item.lastName, value: item.participation}) 
