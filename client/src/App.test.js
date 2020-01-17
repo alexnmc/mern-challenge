b1a5c57fd0319ff200a4ac27a6e9054ebfa4259b
alex <nemechekalexander@gmail.com>
2020-01-16 20:04:03 -0600

    update

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -5,6 +5,7 @@ import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() })
 
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
